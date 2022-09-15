5f7d914769247fdfdcc5f1e896647bf39addadaf
Aman <amythapa3032@gmail.com>
2022-09-15 23:39:47 +0530

    sorting method

--- The Famous Fifty/remove-duplicates.py
+++ The Famous Fifty/remove-duplicates.py
@@ -5,9 +5,24 @@ an array that contains the values of arr without duplicates
 '''
 # brute force [T - O(n^2)]
 
+'''
 def removeDuplicates(arr):
     noDuplicateArr = []
     for element in arr:
         if element not in noDuplicateArr: # traverse again and again
             noDuplicateArr.append(element)
+    return noDuplicateArr
+
+'''
+
+# sorting method [T - O(nlogn)]
+
+def removeDuplicates(arr):
+    if len(arr) == 0: # since we are reading the first element 
+        return []
+    arr.sort()
+    noDuplicateArr = [arr[0]]
+    for i in range (1, len(arr)):
+        if arr[i] != arr[i-1]:
+            noDuplicateArr.append(arr[i])
     return noDuplicateArr
