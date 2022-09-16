674a0254c98f8634e8a0fd10c5cb4ba05e880db9
Aman <amythapa3032@gmail.com>
2022-09-16 23:39:45 +0530

    brute force method

---
+++ The Famous Fifty/find-duplicate.py
@@ -0,0 +1,17 @@
+'''
+Given an array of integers arr that contains n+1 elements
+between 1 and n inclusive, create a function that returns
+the duplicate element (the element that appears more than once)
+
+Assume that:
+- there is only one duplicate number
+- the duplicate number can be repeated more than once
+'''
+
+#brute force [T - O(n^2) ]
+
+def findDuplicate(arr):
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] == arr[j]:
+                return arr[i]
