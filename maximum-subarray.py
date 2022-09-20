34dd87a5d7c2e3e28c1d5e4a3ed84a433d242cb6
Aman <amythapa3032@gmail.com>
2022-09-20 23:36:59 +0530

    cumulative sum approach

--- maximum-subarray.py
+++ maximum-subarray.py
@@ -20,3 +20,17 @@ def maximumSubarray(arr):
             maxSum = max(maxSum, actualSum)
     return maxSum
 '''
+
+# brute force solution with cumulative sum
+# T - O(n^2)
+
+'''
+def maximumSubarray(arr):
+    maxSum = float("-inf")
+    for i in range(len(arr)):
+        cumulativeSum = 0
+        for j in range(i, len(arr)):
+            cumulativeSum += arr[j]
+            maxSum = max(maxSum, cumulativeSum)
+    return maxSum
+'''
