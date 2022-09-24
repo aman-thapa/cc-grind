371bb884af328e50f436a2a4315fd88869741bf8
Aman <amythapa3032@gmail.com>
2022-09-24 21:52:51 +0530

    day-8(brute force)

---
+++ The Famous Fifty/longest-substring-without-repeating-char.py
@@ -0,0 +1,31 @@
+'''
+Given a string str made of alphabetical letters only,
+create a function that returns the length of the longest 
+substring without repeating characters.
+'''
+
+# brute force [T - O(n^3)]
+from unittest import result
+
+
+def withoutrepeating(str):
+    visited = [False] * 128 #including 128 ASCII characters
+    for ch in str:
+        if visited[ord(ch)]: # using ord function to get the equivalent element of a character
+            return False
+        else:
+            visited[ord(ch)] = True
+    return True
+
+def longestSubstringWithoutRepeating(str):
+    maxLength = 0
+    for i in range(len(str)):
+        for j in range(i, len(str)):
+            substr = str[i:j+1]
+            if withoutrepeating(substr) and len(substr) > maxLength:
+                maxLength = len(substr)
+    return maxLength
+
+str1 = "abcdeghf"
+result = longestSubstringWithoutRepeating(str1)
+print(result)
