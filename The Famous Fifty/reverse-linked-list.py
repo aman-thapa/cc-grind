0718f052cf51ae384c4b8a9b29012e38b0a45186
Aman <amythapa3032@gmail.com>
2022-09-25 22:45:37 +0530

    day-9(iteratively)

--- The Famous Fifty/reverse-linked-list.py
+++ The Famous Fifty/reverse-linked-list.py
@@ -3,6 +3,7 @@
 # Time complexity: O(n²)
 # Space complexity: O(1)
 
+'''
 class Node:
   def __init__(self, data, next = None):
     self.data = data
@@ -25,5 +26,31 @@ def reverseList(list):
       right = right.next
     left.data, right.data = right.data, left.data
     left = left.next
+'''
+
+# By dealing with links (iteratively):
+
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+class Node:
+  def __init__(self, data, next = None):
+    self.data = data
+    self.next = next
+
+class LinkedList:
+  def __init__(self, head = None):
+    self.head = head
+
+def reverseList(list):
+  previous = None
+  current = list.head
+  while current is not None:
+    next = current.next
+    current.next = previous
+    previous = current
+    current = next
+  list.head = previous
+
 
 
