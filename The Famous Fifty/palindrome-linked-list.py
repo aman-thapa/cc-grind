c82fa3900264c37ddf6711f3b3992deaa1b7b0d8
Aman <amythapa3032@gmail.com>
2022-09-28 21:38:33 +0530

    day-11

--- The Famous Fifty/palindrome-linked-list.py
+++ The Famous Fifty/palindrome-linked-list.py
@@ -6,6 +6,7 @@ in constant space complexity.
 
 # T - O(n^2)
 
+'''
 def isPalindromeList(list):
     length = 0
     temp = list.head
@@ -21,3 +22,44 @@ def isPalindromeList(list):
             return False
         left = left.next
     return True
+'''
+
+# better approach [T - O(n)]
+# by reversing the right half then comparing
+
+class Node:
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self, head = None):
+        self.head = head
+        
+def reverseList(head):
+    previous = None
+    current = head
+    while current is not None:
+        next = current.next
+        current.next = previous
+        previous = current
+        current = next
+    return previous
+
+def isPalindromeList(list):
+    slow = fast = list.head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    slow = reverseList(slow)
+    head = list.head
+    while slow:
+        if slow.data != head.data:
+            return False
+        slow = slow.next
+        head = head.next
+    return True
+
+lst1 = [1, 2, 3, 4, 3, 2, 1]
+result = isPalindromeList(lst1)
+print(result)
